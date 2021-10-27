e3ae1a44296b1ff2de6ddeed160c0e5484fd7de0
chegashi <mochegri@student.1337.ma>
2021-10-27 14:49:44 +0100

    edit gnl test

--- exam_02/get_next_line/test.sh
+++ exam_02/get_next_line/test.sh
@@ -1,8 +1,8 @@
 for FILE in $(ls test/*.txt)
 do
-    for i in 1 2 3 4 5 10 32 64 100 1000 10000
+    for i in 1 2 3 4 5 10 32 64 100 1000 10000 100000
     do
-        echo "$FILE __ $i"
+        echo -n  "$FILE : BUFFER_SIZE=$i : "
             gcc get_next_line.c main.c -Wall -Wextra -Werror  -D BUFFER_SIZE=1 -o get_next_line -g -fsanitize=address
             ./get_next_line $FILE > tmp
             difference=`diff $FILE tmp`
