682d490b838271e9693f510198f3892ea164c962
chegashi <mochegri@student.1337.ma>
2021-10-27 20:44:05 +0100

    edit gnl

--- exam_02/get_next_line/test.sh
+++ exam_02/get_next_line/test.sh
@@ -3,7 +3,7 @@ do
     for i in 1 2 3 4 5 10 32 64 100 1000 10000 100000
     do
         echo -n  "$FILE : BUFFER_SIZE=$i : "
-            gcc get_next_line.c main.c -Wall -Wextra -Werror  -D BUFFER_SIZE=1 -o get_next_line -g -fsanitize=address
+            gcc get_next_line.c main.c -Wall -Wextra -Werror  -D BUFFER_SIZE=$i -o get_next_line -g -fsanitize=address
             ./get_next_line $FILE > tmp
             difference=`diff $FILE tmp`
             if [ -z $difference]
